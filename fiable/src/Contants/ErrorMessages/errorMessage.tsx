d42ab02248f7562e45803e8efae21b81d6f3c9b6
Eric Cantorna <etc@dewise.com>
2024-10-16 18:35:30 +0800

    update error message

--- fiable/src/Contants/ErrorMessages/errorMessage.tsx
+++ fiable/src/Contants/ErrorMessages/errorMessage.tsx
@@ -1,5 +1,5 @@
 export const ERROR_MESSAGE_INVALID_INPUT =
-  "Invalid input format. Please use 'x,y DIRECTION'.";
+  "Invalid input format. Please use 'x,y DIRECTION (NORTH|SOUTH|EAST|WEST)'.";
 
 export const ERROR_MESSAGE_INVALID_X_Y_COORDINATES =
   "x and y coordinates must be between 0 and 4.";
